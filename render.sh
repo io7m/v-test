047b27b9071f32a687b60498e1e042d5944ba44c
Mark Raynsford <code@io7m.com>
2018-12-19 18:02:58 +0000

    Use -p

--- render.sh
+++ render.sh
@@ -79,6 +79,6 @@ RENDER_HOST="jenkins-renders@mustard.int.arc7.info"
 RENDER_DIRECTORY="/shared/jenkins-renders/${JOB_BASE_NAME}/${BUILD_ID}/"
 RENDER_TARGET="${RENDER_HOST}:${RENDER_DIRECTORY}"
 
-ssh "${RENDER_HOST}" "mkdir ${RENDER_DIRECTORY}" || exit 1
+ssh "${RENDER_HOST}" "mkdir -p ${RENDER_DIRECTORY}" || exit 1
 
 exec rsync -avz --progress "${OUTPUT}/" "${RENDER_TARGET}"
